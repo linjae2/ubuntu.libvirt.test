f9c728cd21e814c1356f7a4c30542c7587b98f48
Jamie Strandboge <jamie@ubuntu.com>
2010-06-16 14:22:39 -0500

    support save and restore
    Gbp-Pq: 9013-apparmor-lp457716.patch.

--- src/qemu/qemu_security_dac.c
+++ src/qemu/qemu_security_dac.c
@@ -340,7 +340,7 @@ qemuSecurityDACRestoreSecurityAllLabel(virDomainObjPtr vm)
 
 
 static int
-qemuSecurityDACSetSecurityAllLabel(virDomainObjPtr vm)
+qemuSecurityDACSetSecurityAllLabel(virDomainObjPtr vm, const char *stdin_path ATTRIBUTE_UNUSED)
 {
     int i;
 
