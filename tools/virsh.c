bcec05f87f4b56a5b5cc6288803e125926f7b336
Guido Günther <agx@sigxcpu.org>
2012-09-14 22:35:08 +0200

    virsh: Initialize library before calling virResetLastError
    Gbp-Pq: virsh-Initialize-library-before-calling-virResetLast.patch.

--- tools/virsh.c
+++ tools/virsh.c
@@ -19560,6 +19560,9 @@ vshInit(vshControl *ctl)
     /* set up the signals handlers to catch disconnections */
     vshSetupSignals();
 
+    if (virInitialize() < 0)
+        return false;
+
     if (virEventRegisterDefaultImpl() < 0)
         return false;
 
