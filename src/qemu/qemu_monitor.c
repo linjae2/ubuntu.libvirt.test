389edf7e8cbeaec9adf36815f2f6f9a7c5662765
Chuck Short <zulcss@ubuntu.com>
2012-11-19 10:41:02 -0600

    increase timeout for waiting on a Unix socket to avoid
    Gbp-Pq: 9005-increase-unix-socket-timeout.patch.

--- src/qemu/qemu_monitor.c
+++ src/qemu/qemu_monitor.c
@@ -259,7 +259,7 @@ qemuMonitorOpenUnix(const char *monitor, pid_t cpid)
 {
     struct sockaddr_un addr;
     int monfd;
-    int timeout = 3; /* In seconds */
+    int timeout = 30; /* In seconds */
     int ret, i = 0;
 
     if ((monfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
