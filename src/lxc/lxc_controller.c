decd605ebc4b5bfacf011f124ce7d9e0d947a5d3
Stefan Bader <stefan.bader@canonical.com>
2014-06-30 16:08:56 +0200

    Support cgmanager
    Gbp-Pq: add-cgmanager-support.patch.

--- src/lxc/lxc_controller.c
+++ src/lxc/lxc_controller.c
@@ -2166,6 +2166,9 @@ virLXCControllerRun(virLXCControllerPtr ctrl)
     if (virLXCControllerSetupResourceLimits(ctrl) < 0)
         goto cleanup;
 
+    if (virCgroupAddTask(ctrl->cgroup, getpid()) < 0)
+        goto cleanup;
+
     if (virLXCControllerSetupDevPTS(ctrl) < 0)
         goto cleanup;
 
