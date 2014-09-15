d72f776aa209a23f84795cd3de70beac53b0f2ed
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-09-15 18:30:06 -0500

    cgroups: ignore systemd failure
    Gbp-Pq: cgroups-ignore-systemd-failure.

--- src/util/virsystemd.c
+++ src/util/virsystemd.c
@@ -251,8 +251,10 @@ int virSystemdCreateMachine(const char *name,
                           3,
                           "Slice", "s", slicename,
                           "After", "as", 1, "libvirtd.service",
-                          "Before", "as", 1, "libvirt-guests.service") < 0)
+                          "Before", "as", 1, "libvirt-guests.service") < 0) {
+        ret = -2;
         goto cleanup;
+    }
 
     ret = 0;
 
