f6b4ce981639d9064b82f1c85843ddd36ec3c6ef
Guido Günther <agx@sigxcpu.org>
2014-10-14 18:34:38 +0200

    Use sensible-editor as fallback
    Gbp-Pq: debian/Use-sensible-editor-as-fallback.patch.

--- tools/virsh.c
+++ tools/virsh.c
@@ -785,7 +785,7 @@ vshEditFile(vshControl *ctl, const char *filename)
     if (!editor)
         editor = virGetEnvBlockSUID("EDITOR");
     if (!editor)
-        editor = "vi"; /* could be cruel & default to ed(1) here */
+        editor = "sensible-editor";
 
     /* Check that filename doesn't contain shell meta-characters, and
      * if it does, refuse to run.  Follow the Unix conventions for
