ea9620363d193af9a80911dbd08a8a16a559159b
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-01-30 10:02:20 +0100

    Support cgmanager
    Gbp-Pq: add-cgmanager-support.patch.

--- src/lxc/lxc_cgroup.c
+++ src/lxc/lxc_cgroup.c
@@ -480,7 +480,7 @@ virCgroupPtr virLXCCgroupCreate(virDomainDefPtr def)
                             true,
                             def->uuid,
                             NULL,
-                            getpid(),
+                            -1,
                             true,
                             def->resource->partition,
                             -1,
