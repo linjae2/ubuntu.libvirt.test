343ac455b4026b67d8b4bdb96fa66b3bd5960b98
Stefan Bader <stefan.bader@canonical.com>
2015-02-13 17:57:27 +0100

    9032-lxc-allow-no-security-driver
    No DEP3 Subject or Description header found
    
    Gbp-Pq: 9032-lxc-allow-no-security-driver.

--- src/lxc/lxc_driver.c
+++ src/lxc/lxc_driver.c
@@ -1565,6 +1565,11 @@ lxcSecurityInit(virLXCDriverConfigPtr cfg)
                                                       cfg->securityDefaultConfined,
                                                       cfg->securityRequireConfined);
     if (!mgr)
+        mgr = virSecurityManagerNew(NULL, LXC_DRIVER_NAME, false,
+                                                      cfg->securityDefaultConfined,
+                                                      cfg->securityRequireConfined);
+
+    if (!mgr)
         goto error;
 
     return mgr;
