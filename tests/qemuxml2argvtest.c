cc279ba3f5c8ff72f2f7a9004e3d8ee04341eb20
Guido Günther <agx@sigxcpu.org>
2015-05-08 18:27:06 +0200

    caps: Fix regression defaulting to host arch
    Gbp-Pq: fromupstream/caps-Fix-regression-defaulting-to-host-arch.patch.

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -595,6 +595,8 @@ mymain(void)
     DO_TEST("machine-usb-opt", QEMU_CAPS_MACHINE_OPT,
             QEMU_CAPS_MACHINE_USB_OPT);
     DO_TEST("kvm", QEMU_CAPS_MACHINE_OPT);
+    DO_TEST("default-kvm-host-arch", QEMU_CAPS_MACHINE_OPT);
+    DO_TEST("default-qemu-host-arch", QEMU_CAPS_MACHINE_OPT);
     DO_TEST("boot-cdrom", NONE);
     DO_TEST("boot-network", NONE);
     DO_TEST("boot-floppy", NONE);
