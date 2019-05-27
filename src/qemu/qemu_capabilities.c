c7b869b89affc9da908f728517a0bebe563dd6cc
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2019-05-27 11:52:07 +0200

    ubuntu/enable-kvm-spice.patch
    No DEP3 Subject or Description header found
    
    Gbp-Pq: ubuntu/enable-kvm-spice.patch.

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -1015,6 +1015,7 @@ virQEMUCapsInitGuest(virCapsPtr caps,
         const char *kvmbins[] = {
             "/usr/libexec/qemu-kvm", /* RHEL */
             "qemu-kvm", /* Fedora */
+            "kvm-spice", /* qemu-kvm-spice Ubuntu package */
             "kvm", /* Debian/Ubuntu */
             NULL,
         };
