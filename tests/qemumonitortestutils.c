3f68ad78d667d3263b62ed0c70947694c69a80f4
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2019-05-28 08:23:25 +0200

    [PATCH] qemu: Adaptive timeout for connecting to monitor
    Gbp-Pq: ubuntu/bug-1705132-qemu-Adaptive-timeout-for-connecting-to-monitor.patch.

--- tests/qemumonitortestutils.c
+++ tests/qemumonitortestutils.c
@@ -901,6 +901,7 @@ qemuMonitorTestNew(bool json,
     if (!(test->mon = qemuMonitorOpen(test->vm,
                                       &src,
                                       json,
+                                      0,
                                       &qemuMonitorTestCallbacks,
                                       driver)))
         goto error;
