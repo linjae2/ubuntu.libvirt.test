a1dee9b63951bba6cd7a97d3414797d984cd0a71
Jonathan Wiltshire <jmw@debian.org>
2019-08-10 13:31:18 +0100

    api: disallow virConnectGetDomainCapabilities on read-only
    Gbp-Pq: security/api-disallow-virConnectGetDomainCapabilities-on-read-only.patch.

--- src/libvirt-domain.c
+++ src/libvirt-domain.c
@@ -11361,6 +11361,7 @@ virConnectGetDomainCapabilities(virConnectPtr conn,
     virResetLastError();
 
     virCheckConnectReturn(conn, NULL);
+    virCheckReadOnlyGoto(conn->flags, error);
 
     if (conn->driver->connectGetDomainCapabilities) {
         char *ret;
