bfe10d9ff8daad881d09b98dcf2abce169a2707f
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2020-08-25 14:53:26 +0200

    Add paths to "ms" variants of OVMF code/vars
    Gbp-Pq: ubuntu/ovmf_paths.patch.

--- src/qemu/qemu_conf.c
+++ src/qemu/qemu_conf.c
@@ -99,7 +99,8 @@ qemuDriverUnlock(virQEMUDriverPtr driver)
     "/usr/share/OVMF/OVMF_CODE.fd:/usr/share/OVMF/OVMF_VARS.fd:" \
     "/usr/share/OVMF/OVMF_CODE.secboot.fd:/usr/share/OVMF/OVMF_VARS.fd:" \
     "/usr/share/AAVMF/AAVMF_CODE.fd:/usr/share/AAVMF/AAVMF_VARS.fd:" \
-    "/usr/share/AAVMF/AAVMF32_CODE.fd:/usr/share/AAVMF/AAVMF32_VARS.fd"
+    "/usr/share/AAVMF/AAVMF32_CODE.fd:/usr/share/AAVMF/AAVMF32_VARS.fd:" \
+    "/usr/share/OVMF/OVMF_CODE.ms.fd:/usr/share/OVMF/OVMF_VARS.ms.fd"
 #endif
 
 
