b43918da67864a1ace89feb6ee965e27b8b733ea
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2020-10-08 07:36:06 +0200

    Extend libvirt checks for ubuntu machine types
    Gbp-Pq: ubuntu/ubuntu_machine_type.patch.

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -8168,6 +8168,7 @@ qemuDomainMachineIsI440FX(const char *machine,
         STRPREFIX(machine, "pc-0.") ||
         STRPREFIX(machine, "pc-1.") ||
         STRPREFIX(machine, "pc-i440fx-") ||
+        STREQ(machine, "ubuntu") ||
         STRPREFIX(machine, "rhel")) {
         return true;
     }
