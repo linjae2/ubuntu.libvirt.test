8e9f01467c416349c6697405daf9aa1e0197a4ec
Mauricio Faria de Oliveira <mfo@canonical.com>
2024-04-16 14:20:13 -0300

    [PATCH] Fix off-by-one error in udevListInterfacesByStatus
    Gbp-Pq: CVE-2024-1441.patch.

--- src/interface/interface_backend_udev.c
+++ src/interface/interface_backend_udev.c
@@ -220,7 +220,7 @@ udevListInterfacesByStatus(virConnectPtr conn,
         virInterfaceDefPtr def;
 
         /* Ensure we won't exceed the size of our array */
-        if (count > names_len)
+        if (count >= names_len)
             break;
 
         path = udev_list_entry_get_name(dev_entry);
